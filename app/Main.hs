462996d9e57cc41e04ea78edcd2953ae41495d0f
Soeren Berg <soeren.berg@protonmail.com>
2022-09-25 17:06:53 +0200

    Create proper app in Main.hs, remove unused Lib.hs module.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main (main) where
 
-import Lib
+import System.Environment (getArgs)
+import HBrainfuck (evalAndPrint, runRepl)
 
 main :: IO ()
-main = someFunc
+main = do args <- getArgs
+          case length args of
+            0 -> runRepl
+            1 -> evalAndPrint $ args !! 0
+            _ -> putStrLn "Pass no args for REPL, 1 for one-time evaluation."
---
+++ src/HBrainfuck.hs
@@ -0,0 +1,6 @@
+module HBrainfuck
+  ( evalAndPrint
+  , runRepl
+  ) where
+
+import HBrainfuck.Internal.Actions
